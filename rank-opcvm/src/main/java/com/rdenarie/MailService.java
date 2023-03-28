7ac0059529875f8bf12c224fc1f541912672dcf8
Romain Dénarié <romain.denarie@exoplatform.com>
2023-03-28 18:23:50 +0200

    Add report generation

--- rank-opcvm/src/main/java/com/rdenarie/MailService.java
+++ rank-opcvm/src/main/java/com/rdenarie/MailService.java
@@ -6,24 +6,24 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.taskqueue.Queue;
-import com.google.appengine.api.taskqueue.QueueFactory;
-import com.google.appengine.api.taskqueue.RetryOptions;
-import com.google.appengine.api.taskqueue.TaskOptions;
 
+import javax.activation.DataHandler;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
@@ -47,41 +47,56 @@ public class MailService extends HttpServlet {
             Message msg = new MimeMessage(session);
             msg.setFrom(new InternetAddress("noreply@rank-opcvm.appspotmail.com"));
             msg.addRecipient(Message.RecipientType.TO,
-                    new InternetAddress("romain.denarie@gmail.com"));
+                             new InternetAddress("romain.denarie@gmail.com"));
             msg.setSubject("Rank-opcvm import status");
 
-            Entity lastDate=GetDataServlet.getLastDate();
+            Entity lastDate = GetDataServlet.getLastDate();
             Query entityQuery = new Query(Utils.DATA_ENTRY_ENTITY).setAncestor(lastDate.getKey());
             PreparedQuery preparedQuery = datastore.prepare(entityQuery);
             int nbElementForLastDate = preparedQuery.countEntities(FetchOptions.Builder.withDefaults());
 
-            Query.Filter missingValueFilter= new Query.FilterPredicate(Utils.MISSING_VALUES, Query.FilterOperator.EQUAL,
-                    true);
-            Query entityQueryWithProblem = new Query(Utils.DATA_ENTRY_ENTITY).setAncestor(lastDate.getKey()).setFilter(missingValueFilter);
+            Query.Filter missingValueFilter = new Query.FilterPredicate(Utils.MISSING_VALUES, Query.FilterOperator.EQUAL,
+                                                                        true);
+            Query
+                entityQueryWithProblem =
+                new Query(Utils.DATA_ENTRY_ENTITY).setAncestor(lastDate.getKey()).setFilter(missingValueFilter);
             PreparedQuery preparedQueryWithProblem = datastore.prepare(entityQueryWithProblem);
-            int nbElementWithProblem= preparedQueryWithProblem.countEntities(FetchOptions.Builder.withDefaults());
-
+            int nbElementWithProblem = preparedQueryWithProblem.countEntities(FetchOptions.Builder.withDefaults());
 
             Date date = (Date) lastDate.getProperty("date");
             Calendar lastCal = Calendar.getInstance();
             lastCal.setTime(date);
 
-            Entity previousLastDate=GetDataServlet.getPreviousDate(lastDate);
-            entityQuery = new Query(Utils.DATA_ENTRY_ENTITY).setAncestor(previousLastDate.getKey());
-            preparedQuery = datastore.prepare(entityQuery);
-            int nbElementForPreviousLastDate = preparedQuery.countEntities(FetchOptions.Builder.withDefaults());
-
-            date = (Date) previousLastDate.getProperty("date");
-            Calendar previousLastCal = Calendar.getInstance();
-            previousLastCal.setTime(date);
-
             String result = "Dernier import ("+lastCal.get(Calendar.DAY_OF_MONTH) + "/" + (lastCal.get(Calendar.MONTH) + 1) + "/" + lastCal.get(Calendar.YEAR)+") : "+nbElementForLastDate+" éléments importés.\n";
-            result += "Avant dernier import ("+previousLastCal.get(Calendar.DAY_OF_MONTH) + "/" + (previousLastCal.get(Calendar.MONTH) + 1) + "/" + previousLastCal.get(Calendar.YEAR)+") : "+nbElementForPreviousLastDate+" éléments importés.\n";
+            Entity previousLastDate = GetDataServlet.getPreviousDate(lastDate);
+            if (previousLastDate != null) {
+                entityQuery = new Query(Utils.DATA_ENTRY_ENTITY).setAncestor(previousLastDate.getKey());
+                preparedQuery = datastore.prepare(entityQuery);
+                int nbElementForPreviousLastDate = preparedQuery.countEntities(FetchOptions.Builder.withDefaults());
+                date = (Date) previousLastDate.getProperty("date");
+                Calendar previousLastCal = Calendar.getInstance();
+                previousLastCal.setTime(date);
+                result += "Avant dernier import ("+previousLastCal.get(Calendar.DAY_OF_MONTH) + "/" + (previousLastCal.get(Calendar.MONTH) + 1) + "/" + previousLastCal.get(Calendar.YEAR)+") : "+nbElementForPreviousLastDate+" éléments importés.\n";
+            }
+
             result += "Elements avec probleme ("+lastCal.get(Calendar.DAY_OF_MONTH) + "/" + (lastCal.get(Calendar.MONTH) + 1) +
                     "/" + lastCal.get(Calendar.YEAR)+") : "+nbElementWithProblem+"\n";
 
 
             msg.setText(result);
+
+            byte[] report = ReportService.generateReport();
+
+
+            Multipart mp = new MimeMultipart();
+
+            //Add attachments
+            MimeBodyPart attachment = new MimeBodyPart();
+            attachment.setDataHandler(new DataHandler(new ByteArrayDataSource(report, "application/vnd.ms-excel")));
+            attachment.setFileName("report.xlsx");
+            mp.addBodyPart(attachment);
+            msg.setContent(mp);
+
             Transport.send(msg);
         } catch (AddressException e) {
             // ...
--- rank-opcvm/src/main/java/com/rdenarie/ReportService.java
+++ rank-opcvm/src/main/java/com/rdenarie/ReportService.java
@@ -2,9 +2,15 @@ package com.rdenarie;
 
 import com.google.appengine.api.datastore.Entity;
 import jdk.jshell.execution.Util;
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFDrawing;
+import org.apache.poi.xssf.usermodel.XSSFShape;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFSimpleShape;
+import org.apache.poi.xssf.usermodel.XSSFTextBox;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.ByteArrayOutputStream;
@@ -21,7 +27,7 @@ import java.util.stream.Collectors;
 public class ReportService {
   private static final Logger log = Logger.getLogger(ReportService.class.getName());
 
-  public byte[] generateReport() {
+  public static byte[] generateReport() {
 
     // Stream containing excel data
     try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
@@ -33,6 +39,7 @@ public class ReportService {
 
       generateGlobalTab(datas,outputStream, myWorkBook);
       generateCategoriesTab(datas,outputStream, myWorkBook);
+      generatePartenairesTab(datas,outputStream, myWorkBook);
       return outputStream.toByteArray();
     }catch (Exception e) {
       log.fine("Unable to generate xls file");
@@ -53,6 +60,12 @@ public class ReportService {
     int quarter = (calendar.get(Calendar.MONTH) + 1) / 4;
     quarter++;
     String lastCellValue = quarter+"° TR "+year;
+
+    Row trimRow = categoriesSheet.getRow(2);
+    Cell trimCell = trimRow.getCell(0);
+    trimCell.setCellValue("C A T É G O R I E S        "+lastCellValue);
+
+
     datas.stream().collect(
         Collectors.groupingBy(entity -> entity.getProperty(Utils.CATEGORY_MS_PROPERTY),
                               Collectors.collectingAndThen(
@@ -95,6 +108,24 @@ public class ReportService {
     int quarter = (calendar.get(Calendar.MONTH) + 1) / 4;
     quarter++;
     String lastCellValue = quarter+"° TR "+year;
+
+
+
+    XSSFDrawing drawing = (XSSFDrawing)globalSheet.getDrawingPatriarch();
+    XSSFShape textShape =drawing.getShapes().get(0);
+    if (textShape instanceof XSSFSimpleShape){
+      XSSFSimpleShape textbox = (XSSFSimpleShape) textShape;
+      for (int i = 0; i < textbox.getTextParagraphs().size(); i++) {
+        if (i==1) {
+          textbox.getTextParagraphs().get(i).getTextRuns().get(0).setText(lastCellValue);
+        } else if (i==2) {
+          textbox.getTextParagraphs().get(i).getTextRuns().get(0).setText("données du "+calendar.get(Calendar.DAY_OF_MONTH)+"/"+(calendar.get(Calendar.MONTH)+1)+"/"+calendar.get(Calendar.YEAR));
+        }
+      }
+    }
+    Row dateRow = globalSheet.getRow(20);
+    Cell dateCell = dateRow.getCell(19);
+    dateCell.setCellValue(calendar.get(Calendar.DAY_OF_MONTH)+"/"+(calendar.get(Calendar.MONTH)+1)+"/"+calendar.get(Calendar.YEAR));
 
     datas.stream().forEach(entity -> {
       Row row = globalSheet.getRow(currentRow.get());
@@ -178,6 +209,141 @@ public class ReportService {
       cell = row.createCell(19);
       cell.setCellStyle(styleRow.getCell(19).getCellStyle());
       cell.setCellValue(lastCellValue);
+      currentRow.getAndIncrement();
+    });
+
+    myWorkBook.write(outputStream);
+  }
+
+  private static void generatePartenairesTab(List<Entity> datas,ByteArrayOutputStream outputStream, XSSFWorkbook myWorkBook) throws IOException {
+
+    XSSFSheet partnersSheet = myWorkBook.getSheetAt(2);
+    AtomicInteger currentRow = new AtomicInteger(17);
+    Row styleRow = partnersSheet.getRow(16);
+
+    Calendar calendar = Calendar.getInstance();
+    int year = calendar.get(Calendar.YEAR);
+    int quarter = (calendar.get(Calendar.MONTH) + 1) / 4;
+    quarter++;
+    String lastCellValue = quarter+"° TR "+year;
+
+    Row dateRow = partnersSheet.getRow(2);
+    Cell dateCell = dateRow.getCell(11);
+    dateCell.setCellValue("SÉLECTION D'OPCVM PARTENAIRES BOURSORAMA AU "+calendar.get(Calendar.DAY_OF_MONTH)+"/"+(calendar.get(Calendar.MONTH)+1)+"/"+calendar.get(Calendar.YEAR));
+
+    dateRow = partnersSheet.getRow(9);
+    dateCell = dateRow.getCell(19);
+    dateCell.setCellValue(calendar.get(Calendar.DAY_OF_MONTH)+"/"+(calendar.get(Calendar.MONTH)+1)+"/"+calendar.get(Calendar.YEAR));
+
+    dateRow = partnersSheet.getRow(12);
+    dateCell = dateRow.getCell(19);
+    dateCell.setCellValue(lastCellValue);
+    dateRow = partnersSheet.getRow(14);
+    dateCell = dateRow.getCell(19);
+    dateCell.setCellValue(lastCellValue);
+
+    var ref = new Object() {
+      String lastCategory = "";
+    };
+    AtomicInteger partnerRankInCategory = new AtomicInteger(0);
+
+    datas.stream().filter(entity -> entity.getProperty(Utils.TYPE_PROPERTY).toString().equals("partenaire") || entity.getProperty(Utils.TYPE_PROPERTY).toString().equals("boursomarkets")).forEach(entity -> {
+      Row row = partnersSheet.createRow(currentRow.get());
+
+      Cell cell = row.createCell(0);
+      cell.setCellStyle(styleRow.getCell(0).getCellStyle());
+      cell.setCellValue(currentRow.get() - 16);
+
+      cell =row.createCell(1);
+      cell.setCellStyle(styleRow.getCell(1).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.RANK_IN_CATEGORY_PROPERTY).toString());
+
+      cell = row.createCell(2);
+      cell.setCellStyle(styleRow.getCell(2).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.NUMBER_FUNDS_IN_CATEGORY_PROPERTY).toString());
+
+      cell = row.createCell(3);
+      cell.setCellStyle(styleRow.getCell(3).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.NAME_PROPERTY).toString());
+
+      cell = row.createCell(4);
+      cell.setCellStyle(styleRow.getCell(4).getCellStyle());
+      cell.setCellValue(Double.parseDouble(entity.getProperty(Utils.SCORE_FOND_PROPERTY).toString()));
+
+      cell = row.createCell(5);
+      cell.setCellStyle(styleRow.getCell(5).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.MSRATING_PROPERTY).toString());
+
+      cell = row.createCell(6);
+      cell.setCellStyle(styleRow.getCell(6).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.ENTREE_PROPERTY).toString());
+
+      cell = row.createCell(7);
+      cell.setCellStyle(styleRow.getCell(7).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.SORTIE_PROPERTY).toString());
+
+      cell = row.createCell(8);
+      cell.setCellStyle(styleRow.getCell(8).getCellStyle());
+      cell.setCellValue(Double.parseDouble(entity.getProperty(Utils.PRICE_EUR_PROPERTY).toString()));
+
+      cell = row.createCell(9);
+      cell.setCellStyle(styleRow.getCell(9).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.CURRENCY_PROPERTY).toString());
+
+      cell = row.createCell(10);
+      cell.setCellStyle(styleRow.getCell(10).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.TICKET_IN_PROPERTY).toString());
+
+      cell = row.createCell(11);
+      cell.setCellStyle(styleRow.getCell(11).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.TICKET_RENEW_PROPERTY).toString());
+
+      cell = row.createCell(12);
+      cell.setCellStyle(styleRow.getCell(12).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.ID_PROPERTY).toString());
+
+      cell = row.createCell(13);
+      cell.setCellStyle(styleRow.getCell(13).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.COURANT_PROPERTY).toString());
+
+      cell = row.createCell(14);
+      cell.setCellStyle(styleRow.getCell(14).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.GERANT_PROPERTY).toString());
+
+      cell = row.createCell(15);
+      cell.setCellStyle(styleRow.getCell(15).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.CATEGORY_MS_PROPERTY).toString());
+
+      cell = row.createCell(16);
+      cell.setCellStyle(styleRow.getCell(16).getCellStyle());
+      cell.setCellValue(Double.parseDouble(entity.getProperty(Utils.SCORE_CATEGORY).toString()));
+
+      cell = row.createCell(17);
+      cell.setCellStyle(styleRow.getCell(17).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.CATEGORY_RANK_PROPERTY).toString());
+
+      cell = row.createCell(18);
+      cell.setCellStyle(styleRow.getCell(18).getCellStyle());
+      cell.setCellValue(entity.getProperty(Utils.NUMBER_OF_CATEGORIES).toString());
+
+      cell = row.createCell(19);
+      cell.setCellStyle(styleRow.getCell(19).getCellStyle());
+      cell.setCellValue(lastCellValue);
+
+      cell = row.createCell(20);
+      if (ref.lastCategory.equals("") || !ref.lastCategory.equals(entity.getProperty(Utils.CATEGORY_MS_PROPERTY))) {
+        cell.setCellValue("1");
+        cell.setCellStyle(styleRow.getCell(20).getCellStyle());
+        partnerRankInCategory.set(1);
+        ref.lastCategory=entity.getProperty(Utils.CATEGORY_MS_PROPERTY).toString();
+      } else {
+        int currentRank = partnerRankInCategory.incrementAndGet();
+        cell.getCellStyle().cloneStyleFrom(styleRow.getCell(20).getCellStyle());
+        cell.getCellStyle().setBorderTop(BorderStyle.NONE);
+        cell.setCellValue(currentRank);
+
+      }
+
       currentRow.getAndIncrement();
     });
 
